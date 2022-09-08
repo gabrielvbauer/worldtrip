e6b9a2544dfacd9d15c5290af106c981e66f1184
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-07 22:29:25 -0300

    project init

---
+++ styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      "500": "#FFBA08",
+      "500-50": "#FFBA0850"
+    },
+    gray: {
+      "600": "#47585B",
+      "400": "#999999",
+      "400-50": "#99999950"
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    heading_alt: 'Barlow',
+    body: 'Poppins',
+    body_alt: 'Barlow',
+  },
+  styles: {
+    global: {
+      body: {
+        color: 'gray.600'
+      }
+    }
+  }
+})
