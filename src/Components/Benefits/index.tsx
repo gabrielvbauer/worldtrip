be340e11193bdff236ed6e31215472dab5e4daaf
gabriel bauer <gabriel.bauer9@hotmail.com>
2022-09-14 13:14:07 -0300

    feat: home responsiveness

--- src/Components/Benefits/index.tsx
+++ src/Components/Benefits/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Text, useBreakpointValue, Box } from "@chakra-ui/react";
 
 interface BenefitsProps {
   title: string;
@@ -6,10 +6,26 @@ interface BenefitsProps {
 }
 
 export function Benefits({ title, icon }: BenefitsProps) {
+  const isMobile = useBreakpointValue({
+    base: true,
+    md: false
+  })
+
   return (
-    <Flex minW="158px" direction="column" align="center">
-      <Image src={icon} alt="cocktail"/>
-      <Text fontSize="1.5rem" fontWeight="semibold" mt="1.5rem">{title}</Text>
+    <Flex minW="158px" direction={{base: 'row', md: 'column'}} align="center" justify="center">
+      {!isMobile &&
+        <Image src={icon} alt="cocktail" mb="1.5rem"/>
+      }
+      {isMobile &&
+        <Box
+          w="8px"
+          h="8px"
+          bg="yellow.500"
+          borderRadius={"full"}
+          mr="8px"
+        />
+      }
+      <Text fontSize="1.5rem" fontWeight="semibold">{title}</Text>
     </Flex>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,38 +12,38 @@ export default function Home() {
       </Head>
       <Header />
       <Flex bgImage="images/Background.png" bgRepeat="no-repeat" bgSize="cover" minH="335px">
-        <HStack maxW="1160px" w="100%" mx="auto" justify="space-between">
+        <HStack maxW="1160px" w="100%" mx="auto" justify="space-between" px="6">
           <Stack spacing="20px" maxW="524px">
             <Text
               color="white.500"
-              fontSize="2.25rem"
+              fontSize={["1.25rem", "2.25rem" ]}
               fontWeight="medium"
             >
               5 Continentes,<br />infinitas possibilidades.
             </Text>
             <Text
               color="white.400"
-              fontSize="1.25rem"
+              fontSize={["sm", "20"]}
               fontWeight="regular"
             >
               Chegou a hora de tirar do papel a viagem que você sempre sonhou.
             </Text>
           </Stack>
-          <Image style={{rotate: '1deg', marginBottom: '-110px'}} src="images/Airplane.svg" alt="Airplane"/>
+          <Image style={{rotate: '1deg', marginBottom: '-110px'}} src="images/Airplane.svg" alt="Airplane" display={["none", "none", "block"]}/>
         </HStack>
       </Flex>
-      <Box maxW="1160px" mx="auto" mb="80px" mt="115px">
-        <HStack justify="space-between">
+      <Box maxW="1160px" mx="auto" mb="80px" mt="115px" px="6">
+        <HStack justify={["center", "space-between"]} wrap="wrap" rowGap="24px">
           <Benefits title='vida noturna' icon='images/cocktail.svg' />
           <Benefits title='praia' icon='images/surf.svg' />
           <Benefits title='moderno' icon='images/building.svg' />
           <Benefits title='clássico' icon='images/museum.svg' />
-          <Benefits title='e mai...' icon='images/earth.svg' />
+          <Benefits title='e mais...' icon='images/earth.svg' />
         </HStack>
         <Center mt="5rem">
           <Divider w="90px" border="2px" borderColor="gray.600" orientation='horizontal' />
         </Center>
-        <Text my="3.25rem" fontSize="2.25rem" fontWeight="medium" textAlign='center'>Vamos nessa?<br />Então escolha seu continente</Text>
+        <Text my="3.25rem" fontSize={["1.25rem", "2.25rem"]} fontWeight="medium" textAlign='center'>Vamos nessa?<br />Então escolha seu continente</Text>
         <Flex minH="28.125rem">
           <Link href="/continent">
             <Center>
